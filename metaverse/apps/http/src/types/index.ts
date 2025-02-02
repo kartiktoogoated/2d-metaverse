65fadac8922f4637b8c8e7c6e890df782c664c15
Kartik Tomar <kartiktomar112233@gmail.com>
2025-02-03 02:29:54 +0530

    schemas in zod

---
+++ metaverse/apps/http/src/types/index.ts
@@ -0,0 +1,56 @@
+import z from "zod"
+
+export const SignupSchema = z.object({
+    username: z.string().email(),
+    password: z.string().min(8),
+    type: z.enum(["admin","user"]),
+})
+
+export const SigninSchema = z.object({
+    username: z.string().email(),
+    password: z.string().min(8),
+})
+
+export const UpdateMetadataSchema = z.object({ 
+    avatarId: z.string()
+})
+
+export const CreateSpacesSchema = z.object({
+    name: z.string(),
+    dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
+    mapId: z.string()
+})
+
+export const AddElementSchema = z.object({
+    name: z.string(),
+    elementId: z.string(),
+    x: z.number(),
+    y: z.number(),
+})
+
+export const CreateElementSchema = z.object({
+    imageUrl: z.string(),
+    width: z.number(),
+    height: z.number(),
+    static: z.boolean(), 
+})
+
+export const UpdateElementSchema = z.object({
+    imageUrl: z.string(),
+})
+
+export const createAvatarSchema = z.object({
+    name: z.string(),
+    imageUrl: z.string(),
+})
+
+export const CreateMapSchema = z.object({
+    thumbnail: z.string(),
+    dimensions: z.string().regex(/^[0-9]{1,4}x[0-9]{1,4}$/),
+    defaultElements:z.array(z.object({
+        elementId: z.string(),
+        x: z.number(),  
+        y: z.number(),
+    }))
+
+})
